c5215e300a38584c7285fdb0293ec264321c74ca
evilbinary <rootntsd@gmail.com>
2023-06-06 01:42:58 +0800

    ref page flag

--- arch/armv7-a/mm.h
+++ arch/armv7-a/mm.h
@@ -45,14 +45,13 @@
 
 #define PAGE_P   0
 #define PAGE_R  0
-#define PAGE_RX   0 //读执行
-#define PAGE_RWX   (L2_TEXT_1 | L2_NCB) //读/写/执行
+#define PAGE_RX   (L2_TEXT_1 | L2_NCB) //读执行
+#define PAGE_RW   (L2_NCB)
+#define PAGE_RWX  (L2_TEXT_1 | L2_NCB) //读/写/执行
 
 #define PAGE_SYS   (L2_TEXT_1|L2_CB) //系统级
 #define PAGE_USR   (L2_TEXT_1|L2_CB) //用户级
 #define PAGE_DEV   (L2_TEXT_0|L2_NCNB) //设备级
-
-#define PAGE_RW   (L2_NCB)
 
 
 
