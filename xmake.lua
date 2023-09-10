018fba81a54065ef3259a358773e0c4b30488b5f
evilbinary <rootntsd@gmail.com>
2023-09-10 21:32:10 +0800

    add cflags

--- xmake.lua
+++ xmake.lua
@@ -1,3 +1,11 @@
 
+--cflags 
+
+for index, value in ipairs(modules) do
+    mod=string.upper(value)..'_MODULE '
+    add_defines(mod)
+    -- add_ldflags(mod)
+end
+
 includes("*/xmake.lua")
 includes("./libs/*/xmake.lua")
