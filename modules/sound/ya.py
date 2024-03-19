230fb376ada18e0ed9a2275541b7cf27c6d5020f
evilbinary <rootntsd@gmail.com>
2024-03-20 00:35:46 +0800

    add build sound

--- modules/sound/ya.py
+++ modules/sound/ya.py
@@ -12,8 +12,60 @@ add_deps(
 )
 
 
+arch=get_arch()
+arch_type=get_arch_type()
+plat=get_plat()
+
+plat_source={
+    'v3s':[
+        'v3s.c',
+    ],
+    'raspi2':[
+        
+    ],
+    'rk3128':[
+        
+    ],
+    'stm32':[
+        'stm32.c'
+    ],
+    'general':[
+        
+    ],
+    'miyoo':[
+       
+    ],
+    't113-s3':[
+    ],
+    'versatilepb':[
+        
+    ],
+    'orangepi-pc':[
+       
+    ]
+    
+}
+arch_source={
+    'arm': [
+        
+    ]
+}
+common_source=[
+    
+]
+
+
+source=[]
+
+if arch_source.get(arch_type):
+    source+=arch_source.get(arch_type)
+
+if plat_source.get(plat):
+    source+=plat_source.get(plat)
+
+
 add_files(
-    './*.c'
+    source+common_source
 )
 
 add_includedirs('../')
