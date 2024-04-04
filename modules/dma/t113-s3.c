8eeefa4a467f5735c204061cca647f1b241f1d74
evilbinary <rootntsd@gmail.com>
2024-04-05 06:38:34 +0800

    add dma

---
+++ modules/dma/t113-s3.c
@@ -0,0 +1,14 @@
+/*******************************************************************
+ * Copyright 2021-present evilbinary
+ * 作者: evilbinary on 01/01/20
+ * 邮箱: rootdebug@163.com
+ ********************************************************************/
+#include "kernel/kernel.h"
+#include "dma.h"
+
+u32 dma_trans(u8 channel, u8 mode, void* addr, void* phyaddr, size_t size) {
+
+  
+
+  return 1;
+}
--- modules/dma/ya.py
+++ modules/dma/ya.py
@@ -11,7 +11,66 @@ add_deps(
     'kernel'
 )
 
+arch=get_arch()
+arch_type=get_arch_type()
+plat=get_plat()
+
+plat_source={
+    'v3s':[
+        'dummy.c',
+    ],
+    'raspi2':[
+        'dummy.c',
+    ],
+    'rk3128':[
+        'dummy.c'
+    ],
+    'stm32':[
+        'dummy.c'
+    ],
+    'general':[
+        'dummy.c'
+    ],
+    'miyoo':[
+        'dummy.c'
+    ],
+    't113-s3':[
+        't113-s3.c'
+    ],
+    'orangepi-pc':[
+        'dummy.c',
+    ],
+    'versatilepb':[
+        'dummy.c'
+    ],
+    'f1c200s':[
+        'dummy.c'
+    ]
+}
+arch_source={
+    'arm': [
+
+    ],
+    'x86':[
+        'dma.c'
+    ]
+}
+common_source=[
+    
+]
+
+
+source=[]
+
+if arch_source.get(arch_type):
+    source+=arch_source.get(arch_type)
+
+if plat_source.get(plat):
+    source+=plat_source.get(plat)
+
 
 add_files(
-    'dma.c'
+    source+common_source
 )
+
+add_includedirs('../')
