5b33be6fb24c587d94ecc2fe677634e527174677
evilbinary <rootntsd@gmail.com>
2024-04-05 22:07:24 +0800

    ref types

--- libs/include/types.h
+++ libs/include/types.h
@@ -45,8 +45,12 @@ typedef signed _Int64   int64_t;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
-typedef unsigned long uint64_t;
 
+
+#if defined(__NEED_uint64_t) && !defined(__DEFINED_uint64_t)
+typedef unsigned _Int64 uint64_t;
+#define __DEFINED_uint64_t
+#endif
 
 #ifndef _INTPTR_T
 #define _INTPTR_T
