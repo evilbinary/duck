4c9d8253890ff9bfd3a8667f2ee821446c5648a2
evilbinary <rootntsd@gmail.com>
2024-07-07 15:30:04 +0800

    add dummy power

---
+++ modules/power/dummy.c
@@ -0,0 +1,13 @@
+/*******************************************************************
+* Copyright 2021-present evilbinary
+* 作者: evilbinary on 01/01/20
+* 邮箱: rootdebug@163.com
+********************************************************************/
+#include "kernel/kernel.h"
+
+
+void power_init_device(device_t* dev){
+
+    
+
+}
--- modules/power/ya.py
+++ modules/power/ya.py
@@ -12,6 +12,67 @@ add_deps(
 )
 
 
+arch=get_arch()
+arch_type=get_arch_type()
+plat=get_plat()
+
+plat_source={
+ 
+    'raspi2':[
+        'dummy.c',
+    ],
+    'rk3128':[
+        'dummy.c'
+    ],
+    'stm32':[
+        'dummy.c'
+    ],
+    'general':[
+        'dummy.c'
+    ],
+    'miyoo':[
+        'dummy.c'
+    ],
+    't113-s3':[
+        'axp209.c'
+    ],
+    'v3s':[
+        'axp209.c'
+    ],
+    'orangepi-pc':[
+        'axp209.c'
+    ],
+    'versatilepb':[
+        'dummy.c'
+    ],
+    'f1c200s':[
+        'dummy.c'
+    ]
+}
+arch_source={
+    'arm':[ 
+        
+    ],
+    'x86':[
+        
+    ],
+}
+common_source=[
+
+]
+
+
+source=[]
+
+if arch_source.get(arch_type):
+    source+=arch_source.get(arch_type)
+
+if plat_source.get(plat):
+    source+=plat_source.get(plat)
+
+
 add_files(
-    './*.c'
+    source+common_source
 )
+
+add_includedirs('../')
